f88fdd3c9cbdb1b443c713bc9ba1fb511ebe3669
thirteen <thirteen@school.iis.nsk.su>
2014-08-02 09:44:44 +0700

    default images, images by nick

--- controllers/images.js
+++ controllers/images.js
@@ -1,3 +1,27 @@
 exports.upload = function() {
 	this.render();
-}
+};
+
+exports.__default__ = function(id) {
+	var T   = this.thirteen,
+		rsp = T.response,
+		req = T.request;
+
+	rsp.setHeader("Set-Cookie", [
+		T.cookie.serialize('image-id', id, {expires: new Date().add(10 * 1000), path: '/' })
+	]);
+
+	this.render();
+};
+
+exports.by = function(id) {
+	var T   = this.thirteen,
+		rsp = T.response,
+		req = T.request;
+
+	rsp.setHeader("Set-Cookie", [
+		T.cookie.serialize('user-id', id, {expires: new Date().add(10 * 1000), path: '/' })
+	]);
+
+	this.render();
+};
